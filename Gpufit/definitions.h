196d19e1dabb5cc4def2d8a3896a6578284fa497
Mark Bates <markb77@gmail.com>
2018-04-06 15:53:16 +0200

    code cleanup

--- Gpufit/definitions.h
+++ Gpufit/definitions.h
@@ -1,25 +1,35 @@
 #ifndef GPUFIT_DEFINITIONS_H_INCLUDED
-#define GPUFIT_DEFINITIONS_H_INCLUDED
+
+    #define GPUFIT_DEFINITIONS_H_INCLUDED
 
     // Status
-#include <stdexcept>
-#define CUDA_CHECK_STATUS( cuda_function_call ) \
-    if (cudaError_t const status = cuda_function_call) \
-    { \
-        throw std::runtime_error( cudaGetErrorString( status ) ) ; \
-    }
+    #include <stdexcept>
 
-#if defined(USE_CUBLAS) && (defined(_WIN64) || defined(__x86_64__) || defined(__LP64__))
+    #define CUDA_CHECK_STATUS( cuda_function_call ) \
+        if (cudaError_t const status = cuda_function_call) \
+        { \
+            throw std::runtime_error( cudaGetErrorString( status ) ) ; \
+        }
 
-#include "cublas_v2.h"
-#define SOLVE_EQUATION_SYSTEMS() solve_equation_systems_lup()
+    #if (defined(_WIN64) || defined(__x86_64__) || defined(__LP64__)) 
+        #define ARCH_64
+    #endif // (defined(_WIN64) || defined(__x86_64__) || defined(__LP64__))
 
-#else // defined(USE_CUBLAS) && (defined(_WIN64) || defined(__x86_64__) || defined(__LP64__))
+    #if (defined(USE_CUBLAS) && !defined(ARCH_64))
+        #undef USE_CUBLAS
+    #endif // (defined(USE_CUBLAS) && !defined(ARCH_64))
 
-#define cublasHandle_t int
-#include "cuda_gaussjordan.cuh"
-#define SOLVE_EQUATION_SYSTEMS() solve_equation_systems_gj()
+    #if defined(USE_CUBLAS)
 
-#endif // defined(USE_CUBLAS) && (defined(_WIN64) || defined(__x86_64__) || defined(__LP64__))
+        #include "cublas_v2.h"
+        #define SOLVE_EQUATION_SYSTEMS() solve_equation_systems_lup()
+
+    #else // defined(USE_CUBLAS)
+
+        #define cublasHandle_t int
+        #include "cuda_gaussjordan.cuh"
+        #define SOLVE_EQUATION_SYSTEMS() solve_equation_systems_gj()
+
+    #endif // defined(USE_CUBLAS) 
 
 #endif // GPUFIT_DEFINITIONS_H_INCLUDED
