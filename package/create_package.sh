b2290b97dab62ff47044f3b003810cf4cb4054ca
S Barnes <sabarnes@llu.edu>
2019-09-05 15:14:39 -0700

    created .deb file for linux install

--- package/create_package.sh
+++ package/create_package.sh
@@ -26,34 +26,6 @@ then
     exit 3
 fi
 
-# imported file date code from windows
-# date and time from https;/-stackoverflow.com/a/30343827/1536976
-
-#set +v SETLOCAL ENABLEDELAYEDEXPANSION
-
-#@# Use WMIC to retrieve date and time
-##@echo off
-#FOR /F "skip=1 tokens=1-6" %%A IN ('WMIC Path Win32_LocalTime Get #Day^,Hour^,Minute^,Month^,Second^,Year/Format:table') DO (
-#if [ !"$~F" = "" ]
-#then
-#(
-#fi
-
-
-#    export SortDate=$(expr10000 * $F) + 100 * $D + $A
-#    export YEAR=!SortDate:~0,4!
-#    export MON=!SortDate:~4,2!
-#    export DAY=!SortDate:~6,2!
-    # Add 1000000 so as to force a prepended 0 if hours < 10 
-#    export SortTime=$(expr 1000000 + 10000) * $B + 100 * $C + $E
-#    export HOUR=!SortTime:~1,2!
-#    export MIN=!SortTime:~3,2!
-#    export SEC=!SortTime:~5,2!
-#  )
-#)
-
-#export DATECODE=!YEAR!!MON!!DAY!!HOUR!!MIN!
-#echo $DATECODE
 
 # define paths
 
@@ -61,16 +33,18 @@ export BUILD_BASE=${args[0]}
 export VERSION=${args[1]}
 export SOURCE_BASE=${args[2]}
 
-export OUTPUT_NAME=Gpufit_$VERSION$_ubuntu18_build 
-#DATECODE
-export ROOT_INSTALL=$BUILD_BASE/$OUTPUT_NAME
-export OUTPUT_ZIP=$BUILD_BASE/$OUTPUT_NAME.zip
+export OUTPUT_NAME=Gpufit_$VERSION
+export ROOT_INSTALL=$BUILD_BASE/install
+export PACKAGE_INSTALL=$BUILD_BASE/install/$OUTPUT_NAME
+export OUTPUT_ZIP=$ROOT_INSTALL/${OUTPUT_NAME}_linux.zip
+export OUTPUT_DEB=$ROOT_INSTALL/${OUTPUT_NAME}.deb
 
-export PERFORMANCE_TEST_INSTALL=$ROOT_INSTALL/gpufit_performance_test
-export PYTHON_INSTALL=$ROOT_INSTALL/PYTHON
-export x64_MATLAB_INSTALL=$ROOT_INSTALL/matlab64
-export x64_JAVA_INSTALL=$ROOT_INSTALL/java64
-export SDK_INSTALL_ROOT=$ROOT_INSTALL/gpufit_sdk
+export PERFORMANCE_TEST_INSTALL=$PACKAGE_INSTALL/gpufit_performance_test
+export PYTHON_INSTALL=$PACKAGE_INSTALL/PYTHON
+export x64_MATLAB_INSTALL=$PACKAGE_INSTALL/matlab64
+export x64_JAVA_INSTALL=$PACKAGE_INSTALL/java64
+export SDK_INSTALL_ROOT=$PACKAGE_INSTALL/gpufit_sdk
+export DEB_ROOT=$PACKAGE_INSTALL
 
 export x64_BUILD=$BUILD_BASE
 #export x64_BUILD_LIB=$BUILD_BASE/Gpufit
@@ -88,7 +62,7 @@ export MATLAB_SOURCE=$SOURCE_BASE/Gpufit/matlab
 export SDK_README_SOURCE=$SOURCE_BASE/package/sdk_readme.txt
 
 export MANUAL_SOURCE=$SOURCE_BASE/docs/_build/latex/Gpufit.pdf
-export MANUAL_INSTALL=$ROOT_INSTALL/Gpufit_VERSION_Manual.pdf
+export MANUAL_INSTALL=$PACKAGE_INSTALL/Gpufit_VERSION_Manual.pdf
 
 # clean up (if necessary)
 
@@ -96,20 +70,51 @@ if [[ -e "$ROOT_INSTALL" ]];
 then 
     rm -r "$ROOT_INSTALL" 
 fi
-if [[ -e "$OUTPUT_ZIP" ]]; 
-then 
-    rm "$OUTPUT_ZIP" 
-fi
+#if [[ -e "$OUTPUT_ZIP" ]]; 
+#then 
+#    rm "$OUTPUT_ZIP" 
+#fi
+#if [[ -e "$OUTPUT_DEB" ]]; 
+#then 
+#    rm "$OUTPUT_DEB" 
+#fi
 
 # create root folder
 
 echo create root directory
 mkdir $ROOT_INSTALL
+mkdir $PACKAGE_INSTALL
+
+# create .deb package
+echo collect .deb files
+mkdir $DEB_ROOT/DEBIAN
+cp "$SOURCE_BASE/package/control" "$DEB_ROOT/DEBIAN"
+sed -i "s/version_string/${VERSION}/" $DEB_ROOT/DEBIAN/control
+
+mkdir $DEB_ROOT/usr
+mkdir $DEB_ROOT/usr/local
+mkdir $DEB_ROOT/usr/local/lib
+cp "$x64_BUILD/Gpufit/libGpufit.so" "$DEB_ROOT/usr/local/lib"
+cp "$x64_BUILD/Cpufit/libCpufit.so" "$DEB_ROOT/usr/local/lib"
+
+mkdir $DEB_ROOT/usr/local/include
+cp "$SOURCE_BASE/Gpufit/gpufit.h" "$DEB_ROOT/usr/local/include"
+
+mkdir $DEB_ROOT/usr/local/bin
+cp "$x64_BUILD/Gpufit_Cpufit_Performance_Comparison" "$DEB_ROOT/usr/local/bin"
+
+echo creating .deb
+(cd ${DEB_ROOT}/.. && dpkg-deb --build $OUTPUT_NAME)
+
+echo delete temp files
+rm -r $DEB_ROOT
+mkdir $PACKAGE_INSTALL
+mv ${OUTPUT_DEB} ${PACKAGE_INSTALL}
 
 # copy main readme (is markdown, written as txt) and license.
 
-cp "$SOURCE_BASE/README.md" "$ROOT_INSTALL/README.txt"
-cp "$SOURCE_BASE/LICENSE.txt" "$ROOT_INSTALL"
+cp "$SOURCE_BASE/README.md" "$PACKAGE_INSTALL/README.txt"
+cp "$SOURCE_BASE/LICENSE.txt" "$PACKAGE_INSTALL"
 
 # copy manual
 
@@ -172,7 +177,7 @@ cp "$x64_BUILD/Gpufit/libGpufit.so" "$SDK_INSTALL_ROOT/linux"
 
 
 # zip content of temp folder with 7-Zip if available
-if 7z a "${OUTPUT_ZIP}" ./"${ROOT_INSTALL}"/*
+if 7z a "${OUTPUT_ZIP}" ./"${PACKAGE_INSTALL}"/*
 then
     #it worked
     echo Created ${OUTPUT_ZIP}
