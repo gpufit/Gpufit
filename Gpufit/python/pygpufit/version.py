bc5a13b1f0762b6e2008064175652b546482ad3a
jkfindeisen <jkfindeisen@users.noreply.github.com>
2021-08-04 12:58:49 +0200

    increment version number to 1.2.0 (needs to be done in 8 distinct places currently)

--- Gpufit/python/pygpufit/version.py
+++ Gpufit/python/pygpufit/version.py
@@ -2,7 +2,7 @@
 Current version (short and full names).
 """
 
-__version_short__ = '1.1'
-__version_full__ = '1.1.0'
+__version_short__ = '1.2'
+__version_full__ = '1.2.0'
 
 __version__ = __version_full__
